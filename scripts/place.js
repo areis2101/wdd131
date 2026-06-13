7699fd1ed7e4d73ae0b134cad6a65ca69ab5e7f3
Alice Reis <alicereisldsrbeiro@gmail.com>
2026-06-13 09:46:44 -0300

    place

--- scripts/place.js
+++ scripts/place.js
@@ -1,20 +1,27 @@
+const currentYear = document.querySelector("#currentyear");
+const lastModified = document.querySelector("#lastModified");
+const windChill = document.querySelector("#windchill");
+
+currentYear.textContent = new Date().getFullYear();
+
+lastModified.textContent =
+    `Last Modified: ${document.lastModified}`;
+
 const temperature = 25;
 const windSpeed = 22;
 
 function calculateWindChill(temp, speed) {
-    return 13.12 + (0.6215 * temp) - (11.37 * Math.pow(speed, 0.16)) + (0.3965 * temp * Math.pow(speed, 0.16));
+    return (
+        13.12 +
+        (0.6215 * temp) -
+        (11.37 * Math.pow(speed, 0.16)) +
+        (0.3965 * temp * Math.pow(speed, 0.16))
+    ).toFixed(1);
 }
-
-let windChill = "N/A";
 
 if (temperature <= 10 && windSpeed > 4.8) {
-    windChill = calculateWindChill(temperature, windSpeed).toFixed(1) + " °C";
+    windChill.textContent =
+        `${calculateWindChill(temperature, windSpeed)} °C`;
+} else {
+    windChill.textContent = "N/A";
 }
-
-document.querySelector("#windchill").textContent = windChill;
-
-document.querySelector("#currentyear").textContent =
-    new Date().getFullYear();
-
-document.querySelector("#lastModified").textContent =
-    `Last Modified: ${document.lastModified}`;
